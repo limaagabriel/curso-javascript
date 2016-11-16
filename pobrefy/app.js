8dce0e892a4440383dc8f7900f8d4e241cf193dd
Gabriel Alves <itsmealves@gmail.com>
2016-11-16 17:14:43 -0300

    post mysql

--- pobrefy/app.js
+++ pobrefy/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var formidable = require("formidable");
+var mysql = require("mysql2");
 
 app.set("view engine", "ejs");
 
@@ -19,14 +20,33 @@ app.get("/cadastro", function(request, response) {
 app.post("/cadastrar", function(request, response) {
 	//ler o formulário
 	var form = formidable.IncomingForm();
+	var connection = mysql.createConnection({
+		host: "localhost",
+		database: "pobrefy",
+		user: "root",
+		password: ""
+	});
 	
 	form.parse(request, function(error, fields) {
-		//	Eu tenho os dados do formulário! YAY
-		console.log(fields.email);
-	});
+		//Eu tenho os dados do formulário! YAY
+		
+		//salvar no banco de dados
+		connection.query("INSERT INTO usuarios" +
+			"(firstName, lastName," +
+			"email, password)" +
+			"VALUES (?,?,?,?)", [fields.firstName,
+			fields.lastName, fields.email,
+			fields.password], function(err) {
+				if(err) {
+					console.log("deu merda");
+				}
+				console.log("inseri!");
+				connection.close();
+			});	
 
-	//salvar no banco de dados
-	//manda pra tela de login
+		//manda pra tela de login
+		response.redirect("/");
+	});
 });
 
 app.listen(3000);
